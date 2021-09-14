bab6eeff0f26f9b6fbd52cc0d9eaa8bcf3816874
jose ramon <rabohopo@gmail.com>
2021-09-14 17:25:57 +0200

    Ended CRUD Moments

--- routes/moments.routes.js
+++ routes/moments.routes.js
@@ -1,14 +1,20 @@
 const router = require("express").Router();
 const Moment = require('./../models/Moment.model')
+const Place = require('./../models/Place.model')
 const { isLoggedIn, checkId } = require("./../middleware")
 const { formatDate } = require('./../utils/index')
 
 //ruta para acceder a la vista index de los momentos
 router.get('/', isLoggedIn, (req, res) => {
 
+
     Moment
         .find()
-        .then(allMoments => res.render('pages/moments/moments',{ allMoments }))
+        .populate('place')
+        .then(allMoments =>{
+            console.log('esto es allmoment',allMoments)
+            res.render('pages/moments/moments',{ allMoments })
+        })
         .catch(err => console.log(err))
        
 })
@@ -21,16 +27,25 @@ router.get('/create', isLoggedIn, (req, res) => res.render('pages/moments/create
 //ruta que recoge los datos del formulario de edición, actualiza los datos y redirige a la lista de momemntos
 router.post('/create', isLoggedIn, (req, res) =>{
 
-    const {date, phrase, lat, lng} = req.body 
 
-    const location = { type: "Point", coordinates: [lat, lng] };
-
+    const { date, phrase, name, lat, lng } = req.body 
+    const location = { type: "Point", coordinates: [ lat, lng ] }
     const owner = req.session.currentUser._id
 
-    Moment
-        .create({ date, phrase, location, owner })
-        .then(() => res.redirect('/moments'))
-        .catch(err => console.log(err))
+
+    Place.create({ name, location })
+    .then(placeCreated => {
+
+       let place = placeCreated._id
+       console.log('esto es placeCreated id',place)
+
+            Moment
+                .create({ date, phrase, place, owner })
+                .then(() => res.redirect('/moments'))
+                .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+
 })
 
 
@@ -38,18 +53,17 @@ router.post('/create', isLoggedIn, (req, res) =>{
 router.get('/:id/edit', isLoggedIn, checkId, (req, res) => {
 
     const {id} = req.params
-    console.log(req.params)
 
     Moment
         .findById(id)
+        .populate('place')
         .then(moment =>{
             
              const { date } = moment
 
             const dateISO = formatDate(date)
-            console.log(dateISO)
 
-            res.render('pages/moments/edit-moment',{ moment, dateISO})
+            res.render('pages/moments/edit-moment',{ moment, dateISO })
 
         })
         .catch(err => console.log(err))
@@ -62,7 +76,16 @@ router.post('/:id/edit', isLoggedIn, checkId, (req, res) => res.redirect('/momen
 
 
 //ruta que recoge los datos del momento a eliminar, elimina el momento y redirige a la lista de momemntos
-router.post('/:id/delete', isLoggedIn, checkId, (req, res) => res.redirect('/moments'))
+router.post('/:id/delete', isLoggedIn, checkId, (req, res) =>{
+    
+    const {id} = req.params
+   
+    Moment
+        .findByIdAndDelete(id)
+        .then(() => res.redirect('/moments'))
+        .catch(err => console.log(err))
+
+})
 
 
 
